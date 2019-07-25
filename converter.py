fe06155993a46b28cd74ca99a1e671d409a6ed21
Robbi Nespu <robbinespu@gmail.com>
2019-07-25 22:00:10 +0800

    remove unwanted comment

--- converter.py
+++ converter.py
@@ -7,8 +7,6 @@ for file in glob.glob("*.pdf"):
     c.xlsx(file,file+".xlsx")
     print("Done!")
 
-#c.xlsx('201806.pdf', '201806.xlsx')
-#c.xlsx('201807.pdf', '201807.xlsx')
 #replace c.xlsx with c.csv to convert to CSV
 #replace c.xlsx with c.xml to convert to XML
 #replace c.xlsx with c.html to convert to HTML
